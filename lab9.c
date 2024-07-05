76c6e7c034b6f4f803c643731aab65bec508597d
Jim Ries <RiesJ@missouri.edu>
2024-07-05 16:15:11 -0500

    Initial Roll-in

---
+++ lab9.c
@@ -0,0 +1,37 @@
+#include "lab9.h"
+#include <stdlib.h>
+
+void * makeArray(int arraySize, int elementSize) 
+{
+	return NULL;
+}
+
+int getSize(void *array) 
+{
+	return 0;
+}
+
+void freeArray(void *array) 
+{
+}
+
+int compareComputersByDiskSize(Computer *a, Computer *b) 
+{
+	return 0;
+}
+
+int compareComputersByCompanyAndModel(Computer *a, Computer *b) 
+{
+	return 0;
+}
+
+
+int searchComputersByDiskSize(Computer *array, Computer *query) 
+{
+	return 0;
+}
+
+int searchComputersByCompanyAndModel(Computer *array, Computer *query) 
+{
+	return 0;
+}
